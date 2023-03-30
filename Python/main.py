855e176f1b22819014fbeaddb84911ca108144f3
Nico Diaz <nicolas0405.nd@gmail.com>
2023-03-30 19:19:48 -0300

    Creamos las clases con sus objetos y dando los metodos

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,15 @@
+from car import Car
+
+def clases():
+    car = Car()
+    car.license = 'AMS234'
+    car.driver = 'Andres Herrera'
+    print(vars(car))
+    
+    car2 = Car()
+    car2.license = 'OUL929'
+    car2.driver = 'Nicolas Diaz'
+    print(vars(car2))
+
 if __name__ == '__main__':
-    pass
+    clases()
